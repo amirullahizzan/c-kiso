0b146361a9e5f5dbd45f0c41d0e09a943dadfa94
Zan <38888951+amirullahizzan@users.noreply.github.com>
2023-06-30 16:09:04 +0900

    Add files via upload

---
+++ 20230630/0301/0301.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "int n " << sizeof n << endl;
+    char c;
+    cout << "char c " << sizeof c << endl;
+    float f;
+    cout << "float f " << sizeof f << endl;
+}
---
+++ 20230630/0303/Char1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void ShowCode(char ch)
+{
+	cout << "「" << ch << "」にふられた番号は "
+		 << (int)(unsigned char)ch << "　です。　" << endl;
+}
+
+int main()
+{
+	ShowCode('0');
+	ShowCode('A');
+}
